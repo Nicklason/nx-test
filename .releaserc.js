802ea0a79b274a02f55064303e985074b9a6400a
Nicklas Marc Pedersen <nicklas.marc.pedersen@gmail.com>
2023-02-11 13:30:31 +0100

    chore: don't push to repo on release

--- .releaserc.js
+++ .releaserc.js
@@ -3,14 +3,6 @@ module.exports = {
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
     '@semantic-release/github',
-    '@semantic-release/changelog',
-    [
-      '@semantic-release/npm',
-      {
-        npmPublish: false,
-      },
-    ],
-    '@semantic-release/git',
   ],
   branches: [
     'main',
